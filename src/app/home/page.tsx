4bd6268cdeee4eec10d35a6c8fded54e0c98abf3
Pedro Lucca Medeiros Miranda <pedroluccaprofissional@gmail.com>
2024-05-28 23:21:00 -0300

    feat: home title

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,11 +7,21 @@ const Home = () => {
     <>
       <Header />
 
+      
+
       <div className={styles.videoContainer}>
+
+        <div className={styles.titleContainer}>
+          <h1>NOS AJUDE A SALVAR O MUNDO</h1>
+        </div>
+
         <video className={styles.video} width="100%" preload='true' autoPlay loop muted playsInline>
           <source src="/ocean.mp4" type="video/mp4"/>
         </video>
       </div>
+
+
+      <div className={styles.break}></div>
 
 
     </>
