8ed0ce3abd599fc6b8582ba0337081408258aee7
Pedro Jesus <pedromra.04@gmail.com>
2024-05-30 00:38:27 -0300

    style: add colors vars

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,70 +1,69 @@
-'use client'
-import React from 'react'
-import Header from '../../components/Header'
-import styles from './styles.module.css'
-import Button from '@/app/components/Button'
-import {useRouter } from 'next/navigation'
-import Card from '@/app/components/Card'
+"use client";
+import React from "react";
+import Header from "../../components/Header";
+import styles from "./styles.module.css";
+import Button from "@/app/components/Button";
+import { useRouter } from "next/navigation";
+import Card from "@/app/components/Card";
 
 const Home = () => {
-
-  const router = useRouter()
+  const router = useRouter();
 
   return (
     <>
       <Header />
 
-
-
       <div className={styles.videoContainer}>
-
         <div className={styles.titleContainer}>
           <h1>A ESPERANÇA É AZUL OCEANO</h1>
           <div className={styles.buttonGrid}>
-          <Button
-            onClick={() => { console.log("FUNCIONOU") }}
-            text='Criar conta'
-          ></Button>
-           <Button
-            onClick={() => {router.push("/login") }}
-            text='Entrar'
-            variant='secondary'
-          ></Button>
+            <Button
+              onClick={() => {
+                console.log("FUNCIONOU");
+              }}
+              text="Criar conta"
+            ></Button>
+            <Button
+              onClick={() => {
+                router.push("/login");
+              }}
+              text="Entrar"
+              variant="secondary"
+            ></Button>
           </div>
-          
         </div>
 
-        <video className={styles.video} width="100%" preload='true' autoPlay loop muted playsInline>
+        <video
+          className={styles.video}
+          width="100%"
+          preload="true"
+          autoPlay
+          loop
+          muted
+          playsInline
+        >
           <source src="/ocean.mp4" type="video/mp4" />
         </video>
       </div>
 
       <div className={styles.break}></div>
 
-
       <h4 className={styles.introduceText}>Com a IA da BlueHope você pode</h4>
 
       <div className={styles.cardGroup}>
-        <Card
-          path='/pin_icon.svg'
-          text='Mapear o Despejo de Plástico'
-        />
+        <Card path="/pin_icon.svg" text="Mapear o Despejo de Plástico" />
 
         <Card
-          path='/lens_icon.svg'
-          text='Identificar Lixo Marinho em Imagens'
+          path="/lens_icon.svg"
+          text="Identificar Lixo Marinho em Imagens"
         />
 
-        <Card
-          path='/pin_icon.svg'
-          text='Mapear Despejo de Plástico'
-        />
+        <Card path="/pin_icon.svg" text="Mapear Despejo de Plástico" />
       </div>
 
       <div className={styles.break}></div>
-
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
