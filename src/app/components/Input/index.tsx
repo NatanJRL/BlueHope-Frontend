f659b17fc5be9b695f3e028b61ca29b40c149356
Pedro Jesus <pedromra.04@gmail.com>
2024-06-01 22:35:56 -0300

    feat: add API request to backend

--- src/app/components/Input/index.tsx
+++ src/app/components/Input/index.tsx
@@ -6,18 +6,19 @@ type Props = {
     type: string,
     name: string,
     id: string,
-    onChange: () => void,
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
     value: string | number,
-    isForLogin?: boolean
+    isForLogin?: boolean,
+    className?: string,
 }
 
-const Input: React.FC<Props> = ({label="", type, name,  id, onChange, value="", isForLogin=false}) => {
+const Input: React.FC<Props> = ({label="", type, name,  id, onChange, value="", isForLogin=false, className=""}) => {
   return (
     <>
         <div className={styles.inputContainer}>
             {isForLogin ? (<label className={styles.labelStyle} htmlFor={label}>{label}</label>
             ): ""}
-            <input onChange={onChange} className={styles.inputStyle} placeholder={isForLogin ? "" : label} type={type} value={value} name={name} id={id} />
+            <input onChange={onChange} className={`${styles.inputStyle} ${className}`} placeholder={isForLogin ? "" : label} type={type} value={value} name={name} id={id} />
         </div>
     </>
 
